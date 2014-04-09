584d00ff7efa036a97084ca17dd62a65413a7366
Luiz Amorim <luiz.simples@gmail.com>
2014-04-09 18:32:22 -0300

    Foi colocado como TaskRunner o Gulp ao invés de Grunt

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+
+gulp.task('server', function() {
+  //TO-DO
+});
+
+gulp.task('tests', function() {
+  //TO-DO
+});
+
+gulp.task('deploy', function() {
+  //TO-DO
+});
